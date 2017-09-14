d4e49689bb17bb9dbc8e72b4271ae19d381ac17f
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-09-14 19:44:51 +0300

    edit admin: add search box and list order

--- videos/admin.py
+++ videos/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 from . import models
 
+
 # Register your models here.
 
-admin.site.register(models.Movie)
+class MovieAdmin(admin.ModelAdmin):
+    fields = ['release_year', 'title', 'length']  # in this order fields will appear
+    search_fields = ['title', 'length']  # create search box
+
+admin.site.register(models.Movie, MovieAdmin)
+
+
 admin.site.register(models.Customer)
